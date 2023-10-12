cb5ff588a3ce9b27b407fb24bb449809ec2ec114
durgesh253 <jadhavdurgesh256@gmail.com>
2023-10-12 13:51:20 +0530

    practice completed

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import React from "react";
-import { Text, View } from "react-native";
-import AppNavigations from "./Navigations/AppNavigations";
-
+import React, { useState } from "react";
+import { SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Icon from "./components/IconName";
+// import Snackbar from "react-native-snackbar";
 
 const App = () => {
-   return (
-    <AppNavigations/>
-   )
+
+
+
+  return (
+    <>
+     
+     
+      <View style={styles.container}>
+      <Icon/>
+      </View>
+      
+
+      </>
+  );
 }
-export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 70,
+  },
+  actionBtn: {
+    borderRadius: 8,
+    backgroundColor: "red",
+    padding: 18,
+  },
+  btnText: {
+    color: "black",
+  }
+});
+
+export default App;
---
+++ Constatnt.js
@@ -0,0 +1,36 @@
+import { Track } from "react-native-track-player";
+
+export const playListData = [
+  {
+    id: '1',
+    title: 'Man Meri Jaan',
+    artist: 'King',
+    album: 'Champagne Talk',
+    artwork: 'https://upload.wikimedia.org/wikipedia/en/4/4f/Aashiqui_2.jpeg',
+    url: require('./assets/play1.mp3'),
+  },
+  {
+    id: '2',
+    title: 'Another Song Title',
+    artist: 'Another Artist',
+    album: 'Another Album',
+    artwork: 'https://example.com/another-image.jpg',
+    url: require('./assets/play2.mp3'),
+  },
+  {
+    id: '3',
+    title: 'Yet Another Song',
+    artist: 'Yet Another Artist',
+    album: 'Yet Another Album',
+    artwork: 'https://example.com/yet-another-image.jpg',
+    url: require('./assets/play3.mp3'),
+  },
+  {
+    id: '4',
+    title: 'One More Song',
+    artist: 'One More Artist',
+    album: 'One More Album',
+    artwork: 'https://example.com/one-more-image.jpg',
+    url: require('./assets/play4.mp3'),
+  }
+];
